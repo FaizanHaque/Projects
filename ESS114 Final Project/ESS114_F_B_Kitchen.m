df24309f0cec7d384f252a3c2af1f4aed8d88ff1
FaizanHaque <sfhaque@uci.edu>
2018-06-07 00:30:49 -0700

    fixed cLayers

--- ESS114 Final Project/ESS114_F_B_Kitchen.m
+++ ESS114 Final Project/ESS114_F_B_Kitchen.m
@@ -1,14 +1,21 @@
 tic
-B_k = xlsread('bryandata_neat.xlsx');
+B_k = xlsread('cesar_neat.xlsx');
 toc
 tic
-tEnd = 4;
-yDis= 23 ;
-xDis= 23;
+tEnd = 3;
+cLayers = 60:125;
 
-%yDis= 22.5 ;
-%xDis= 14.5;
-B_k(:,5) = (B_k(:,5)+23)/xDis;
+%yDis= 23 ;
+%xDis= 23;
+%cTitles = ["Absolute Magnetic Field Strength at 11:12pm Sunday t = 78°F ","Absolute Magnetic Field Strength at 2:04pm t = Monday 77F°"," Absolute Magnetic Field Strength at 11:11pm Monday 75F","Absolute Magnetic Field Strength at 3:20 pm 77 F"];
+%qTitles = ["Magnetic Field at 11:12pm Sunday t = 78°F ","Magnetic Field at 2:04pm Monday t = 77°F","Magnetic Field at 11:11pm Monday t = 75°F","Magnetic Field at 3:20 pm t = 77°F"];
+
+yDis= 22.5 ;
+xDis= 14.5;
+cTitles = ["Absolute Magnetic Field Strength at 11:12pm Sunday t = 78°F ","Absolute Magnetic Field Strength at 2:04pm t = Monday 77F°"," Absolute Magnetic Field Strength at 11:11pm Monday 75F"];
+qTitles = ["Magnetic Field at 11:12pm Sunday t = 78°F ","Magnetic Field at 2:04pm Monday t = 77°F","Magnetic Field at 11:11pm Monday t = 75°F"];
+
+B_k(:,5) = (B_k(:,5)+xDis)/xDis;
 B_k(:,6) = (B_k(:,6))/yDis;
 Bx = zeros(5,5,tEnd);
 By = zeros(5,5,tEnd);
@@ -31,7 +38,6 @@ Babs_temp = accumarray([B_k(((t-1)*25+1):t*25,5),B_k(((t-1)*25+1):t*25,6)],B_k((
 Babs(:,:,t)= Babs_temp;
 
 end
-%B_k(:,5) = (B_k(:,5)+23)/23;
 X = ones(5,5);
 Y = ones(5,5);
 Z = zeros(5,5);
@@ -40,13 +46,10 @@ X(i,:) = i*xDis;
 Y(:,i) = i*yDis;
 end
 
-cTitles = ["Absolute Magnetic Field Strength at 11:12pm Sunday 78F ","Absolute Magnetic Field Strength at 2:04pm Monday 77F"," Absolute Magnetic Field Strength at 11:11pm Monday 75F","Absolute Magnetic Field Strength at 3:20 pm 77 F"];
-qTitles = ["Magnetic Field at 11:12pm Sunday 78F ","Magnetic Field at 2:04pm Monday 77F","Magnetic Field at 11:11pm Monday 75F","Magnetic Field at 3:20 pm 77 F"];
 
-cLayers = 60:125;
+
 for time = 1:tEnd
 figure(time)
-%    zlim([-3 3])
 
 quiver3(X,Y,Z,Bx(:,:,time),By(:,:,time),Bz(:,:,time),0.3);
 zticks([]);
@@ -57,7 +60,6 @@ ylabel('cm');
 rotate3d on
 title(qTitles(time));
 
-%drawnow
 
 figure(time + 4)
 [C,h]=contourf(X,Y,Babs(:,:,time),cLayers ,'edgecolor','none');
@@ -66,8 +68,7 @@ colormap(flipud(hot));
 xlabel('cm');
 ylabel('cm');
 clabel(C,h);
-ylabel(C_bar,'Magnetic field strenght (?Tesla)')
-%ylabel(C,'Magnetic field strenght (?Tesla)', 'FontSize',15);
+ylabel(C_bar,'Magnetic field strenght (µTesla)')
 
 title(cTitles(time));
 
